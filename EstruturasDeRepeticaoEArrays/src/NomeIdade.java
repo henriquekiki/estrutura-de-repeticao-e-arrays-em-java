e5d8cf3b64227c1e0a24aa029c9d3bb9ba50d1e8
henriquekiki <ricckdmo@gmail.com>
2023-01-20 13:37:06 -0300

    Exercicios

---
+++ EstruturasDeRepeticaoEArrays/src/NomeIdade.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NomeIdade {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true){
+            System.out.println("Digite seu nome: ");
+            nome = scan.next();
+            if (nome.equals("0")) break;
+            System.out.println("Digite sua idade: ");
+            idade = scan.nextInt();
+            //if (idade > 100) break;
+        }
+
+        
+
+
+    }
+}
